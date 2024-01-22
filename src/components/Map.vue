87615a2488b555950ec5ff358ea452777ad0fb9a
Emmaline Rial <emr102@pitt.edu>
2024-01-22 02:27:28 -0500

    initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+    <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+mapboxgl.accessToken = ""
+
+export default {
+    mounted() {
+  const map = new mapboxgl.Map({
+    container: this.$refs.mapContainer,
+    style: "", // Replace with your preferred map style
+    center: [-87.844, 41.790],
+    zoom: 9,
+  });
+  
+
+  this.map = map;
+},
+unmounted() {
+  this.map.remove();
+  this.map = null;
+}
+};
+</script>
+
+<style>
+.map-container {
+  flex: 1;
+}
+</style>
