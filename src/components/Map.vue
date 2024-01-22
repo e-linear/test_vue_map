15afcd8d93a70f6395968459705936197a0a23ec
Emmaline Rial <emr102@pitt.edu>
2024-01-22 03:22:32 -0500

    Update Map.vue

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,13 +4,13 @@
 
 <script>
 import mapboxgl from "mapbox-gl";
-mapboxgl.accessToken = "pk.eyJ1IjoiZWxpbmVhciIsImEiOiJjbG9jeGRzcWkwMXV4Mm1ueW0wY2g4bWZ3In0.ILk87MZMtJpRO0TzguWD6g"
+mapboxgl.accessToken = os.getenv("MAPBOX_ACCESS_TOKEN")
 
 export default {
     mounted() {
   const map = new mapboxgl.Map({
     container: this.$refs.mapContainer,
-    style: "mapbox://styles/elinear/clrjm447m000701p1e5y7awrc", // Replace with your preferred map style
+    style: os.getenv("MAPBOX_STYLE"), // Replace with your preferred map style
     center: [-87.844, 41.790],
     zoom: 9,
   });
